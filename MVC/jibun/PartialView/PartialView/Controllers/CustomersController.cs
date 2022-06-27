d8c969d65538e6d687f951eecfda5f1367cff080
bwin0415 <v826415629@gmail.com>
2022-06-27 16:33:50 +0800

    z

---
+++ MVC/jibun/PartialView/PartialView/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PartialView.Models;
+
+namespace PartialView.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            NorthwindContext dc = new NorthwindContext();
+            ViewBag.CustomerId = new SelectList(dc.Customers, "CustomerId", "CompanyName");
+            return View();
+        }
+        public async Task<IActionResult> Orders(string id)
+        {
+            NorthwindContext dc = new NorthwindContext();
+            Customers c = await dc.Customers.FindAsync(id);
+            return PartialView("_OrdersPartial",c.Orders);
+
+        }
+    }
+}
---
+++ MVC/jibun/PartialView/PartialView/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$("#CustomerId").bind("change", () => {
+    UpdateOrders();
+})
+function UpdateOrders() {
+    $.ajax({
+        type: "GET",
+        url: webroot +"/"+ CustomerId.value,
+        //url: "/Customers/Orders/" + CustomerId.value,
+    })
+        .done((data) => { $("#Orders").html(data) })
+        .fail((err) => { alert(err.responseText) });
+}
